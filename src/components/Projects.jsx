67d792b78c898df9f1e71f4d913da55368f222ec
Juan Sansberro <121207993+juankadesigns18@users.noreply.github.com>
2025-03-19 09:23:16 -0300

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ const projectsData = [
     description: 'Esta es una página web de documentación de Cita',
     link: 'https://soporte-cita.netlify.app/',
     image: projectImage3,
-    text: '2025',
+    text: En progreso',
   },
   {
     title: 'Services Doc',
